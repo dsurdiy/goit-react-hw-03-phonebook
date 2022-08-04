1e4a18428ed48872868fcc2b56e18bce0c61e333
DimaSurdiy <dsurdiy@ukr.net>
2022-08-04 15:16:37 +0300

    saving contacts to local storage added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,11 +5,29 @@ import { Filter } from 'components/Filter/Filter';
 import { Container, AppTitle, ContactsTitle } from './App.styled';
 import { FcContacts } from 'react-icons/fc';
 
+const LS_KEY = 'saved_contacts';
+
 export class App extends Component {
   state = {
     contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem(LS_KEY));
+
+    if (savedContacts) {
+      this.setState({
+        contacts: savedContacts,
+      });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem(LS_KEY, JSON.stringify(this.state.contacts));
+    }
+  }
 
   formSubmitHandler = newContact => {
     const { contacts } = this.state;
